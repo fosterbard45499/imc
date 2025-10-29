067065fbaab4f5b77471b4403b74e22da3b596bb
fosterbard45499 <145789224+fosterbard45499@users.noreply.github.com>
2025-10-29 18:28:52 -0300

    src ionic+firabes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBRcgXWqocPAJDe_fx-OLY7IPCqXOO26IQ",
+    authDomain: "sistemalogin-8ff92.firebaseapp.com",
+    projectId: "sistemalogin-8ff92",
+    storageBucket: "sistemalogin-8ff92.firebasestorage.app",
+    messagingSenderId: "217821721375",
+    appId: "1:217821721375:web:579fd9fd681a50fea8762a",
+    measurementId: "G-5CS3JEZ31F"
+  }
+};
